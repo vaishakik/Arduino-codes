0e17722cd4eff84bf97b478ae49989513702003d
Vaishakh I Kuppast <vaishak.i.kuppast@gmail.com>
2020-12-30 20:55:02 +0530

    Add files via upload

---
+++ ed1.ino
@@ -0,0 +1,26 @@
+int state=0;
+void setup() {
+  pinMode(11, INPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  state=digitalRead(11);
+  if(state==HIGH)
+  {
+   Serial.print("HIGH\n");
+   delay(500);
+   
+    }
+    else
+    {
+      Serial.print("LOW\n");
+      digitalWrite(12,HIGH);
+      delay(500);
+    
+    }
+
+}
---
+++ nodemcu.ino
@@ -0,0 +1,64 @@
+#include <Firebase.h>
+#include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+
+
+
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial esp(9,10); // RX | TX
+#define FIREBASE_HOST "shiv3-e3f47.firebaseio.com"
+#define FIREBASE_AUTH "Y0554vDycXHABcmJ4hLJKAwCeV17uh7ejdyXdHM"
+String data;
+
+String ssid = "Why?shock! mobile";   //Wi-Fi ID
+String password ="kuppast9"; //Wi-Fi Password
+
+void setup() 
+{
+
+  
+  Serial.begin(9600);
+  esp.begin(9600); //Important, Dont Change
+  Serial.println("Initializing");
+  delay(1000); //Giving time for Wi-Fi module to start
+  Serial.println("Initialized");
+  //Connect to Wi-Fi
+  Serial.print("Connecting to -");
+  Serial.println(ssid);
+ 
+  esp.flush();
+  esp.print("WIFICON|");
+  esp.print(ssid);
+  esp.print(",");
+  esp.print(password);
+  esp.println("|");
+  delay(500);
+ 
+   while (!esp.available()) {// wait for serial respone from Wi-Fi Module
+    Serial.print(".");
+    delay(500);
+  }
+  if(esp.available()){
+    data = esp.readString();
+    delay(150);
+    Serial.println(data);
+  }
+  esp.flush();
+  delay(3000); 
+
+
+   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+   Firebase.set("Testing", 0);
+
+   Firebase.setInt("Testing", 88);
+}
+
+void loop()
+{
+}
+
